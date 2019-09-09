102db0cf7da818c8f397632e77d5f4f2613ba7b5
Dino Mikulan <dino.mikulan@gmail.com>
2019-09-09 11:14:22 +0200

    Signed-off-by: Dino Mikulan <dino.mikulan@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@ x<!DOCTYPE html>
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Dics</a>
+                    <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
